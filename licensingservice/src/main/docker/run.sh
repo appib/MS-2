4a79acf27f6ef8b2761ec669e90fa0c67cecc733
bhiapp4 <bhiapp4@gmail.com>
2018-03-02 12:43:07 -0600

    dockerized the spring boot services

---
+++ licensingservice/src/main/docker/run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "********************************************************"
+echo "Starting Licensing Service"
+echo "********************************************************"
+java -jar /usr/local/licensingservice/@project.build.finalName@.jar
---
+++ organizationservice/src/main/docker/run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "********************************************************"
+echo "Starting Organization Service"
+echo "********************************************************"
+java -jar /usr/local/organizationservice/@project.build.finalName@.jar
